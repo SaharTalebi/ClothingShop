a96ff16cfe208fed09efcf2f75fb565e437052ab
sahar <s.talebi.sut@gmail.com>
2022-07-03 20:06:33 +0430

    create accounts app and CustomUser model

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #My App
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -119,5 +122,5 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-
+AUTH_USER_MODEL = 'accounts.CustomUser'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
