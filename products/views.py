168c87bc009b8fe3a5d3c3007daa6ea2227dd4b0
sahar <s.talebi.sut@gmail.com>
2022-07-05 13:19:27 +0430

    complete the products page for it's links and pagination

--- products/views.py
+++ products/views.py
@@ -1,14 +1,41 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 
 from .models import Product
 # Create your views here.
 
 def product_list_view(request):
-    products = Product.objects.order_by('-created_date')
+    all_products = Product.objects.order_by('-created_date')
+    paginator = Paginator(all_products, 3)
+    page = request.GET.get('page')
+    products = paginator.get_page(page)
+    
+    if 'newest' in request.GET:
+        all_products = Product.objects.order_by('-created_date')
+        paginator = Paginator(all_products, 3)
+        page = request.GET.get('page')
+        products = paginator.get_page(page)
+        
+    elif 'low_price' in request.GET:
+        all_products = Product.objects.order_by('price')
+        paginator = Paginator(all_products, 3)
+        page = request.GET.get('page')
+        products = paginator.get_page(page)
+        
+    elif 'high_price' in request.GET:
+        all_products = Product.objects.order_by('-price')
+        paginator = Paginator(all_products, 3)
+        page = request.GET.get('page')
+        products = paginator.get_page(page)
+        
     context = {
-        'products': products
-    }
+            'products': products,
+            # 'paged_products' : paged_products,
+        }
+
     return render(request, 'products/products.html', context)
+    
+
 
 def product_detail_view(request, pk):
     single_product = get_object_or_404(Product, pk=pk)
